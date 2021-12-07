662fbd3a98ec0df4bfe6cc18c2ef22342386978b
Hung Nguyen <hnguyenmanh@umass.edu>
2021-12-07 17:29:49 -0500

    graphing data

--- project.py
+++ project.py
@@ -8,11 +8,22 @@ from scipy.signal import butter, filtfilt, find_peaks, iirnotch
 matplotlib.style.use('ggplot')
 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier 
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split 
 from sklearn import metrics
 from sklearn import tree
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['Name', 'Age', 'Marks']
+
+df = pd.read_csv("./data/Snoring-2.csv", header=None, squeeze=False)[:20_000]
+df.plot()
+
+plt.show()
 
 
 
