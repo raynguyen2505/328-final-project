5601928686b24c26040aa83d4d54d4a8f237ed54
Hung Nguyen <hnguyenmanh@umass.edu>
2021-12-07 16:54:12 -0500

    some setups and data

---
+++ project.py
@@ -0,0 +1,18 @@
+import matplotlib
+from matplotlib import cm
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt, find_peaks, iirnotch
+
+matplotlib.style.use('ggplot')
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics
+from sklearn import tree
+from sklearn.tree import DecisionTreeRegressor
+
+
+
